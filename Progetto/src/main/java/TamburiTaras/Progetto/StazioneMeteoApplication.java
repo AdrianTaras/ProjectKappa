f7af0d4625dc238e6229abf068e36f701fbb6aae
LorenzoTamburi <75088702+LorenzoTamburi@users.noreply.github.com>
2020-12-22 11:00:32 +0100

    giorno 1

---
+++ Progetto/src/main/java/TamburiTaras/Progetto/StazioneMeteoApplication.java
@@ -0,0 +1,13 @@
+package TamburiTaras.Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StazioneMeteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StazioneMeteoApplication.class, args);
+	}
+
+}
---
+++ Progetto/src/test/java/TamburiTaras/Progetto/StazioneMeteoApplicationTests.java
@@ -0,0 +1,13 @@
+package TamburiTaras.Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StazioneMeteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
